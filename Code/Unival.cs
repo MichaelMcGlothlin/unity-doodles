37d864b7053dba34eb0493eb6f68302c9042c491
Michael McGlothlin <michael@kavlon.com>
2018-10-23 11:01:15 -0500

    Added namespace.

--- Code/Unival.cs
+++ Code/Unival.cs
@@ -1,17 +1,19 @@
-﻿/// <summary>
-/// Unit Interval [0..1].
-/// </summary>
-public struct Unival {
- private System.Single _value;
+﻿namespace Kavlon {
+ /// <summary>
+ /// Unit Interval [0..1].
+ /// </summary>
+ public struct Unival {
+  private System.Single _value;
 
- public Unival ( System.Single value ) {
-  if ( ( value < 0 ) || ( value > 1 ) ) {
-   throw new System.ArgumentOutOfRangeException ( nameof ( value ), "Unit intervals should be between zero and one." );
+  public Unival ( System.Single value ) {
+   if ( ( value < 0 ) || ( value > 1 ) ) {
+    throw new System.ArgumentOutOfRangeException ( nameof ( value ), "Unit intervals should be between zero and one." );
+   }
+   _value = value;
   }
-  _value = value;
+
+  public static implicit operator System.Single ( Unival unival ) => unival._value;
+
+  public static implicit operator Unival ( System.Single single ) => new Unival ( single );
  }
-
- public static implicit operator System.Single ( Unival unival ) => unival._value;
-
- public static implicit operator Unival ( System.Single single ) => new Unival ( single );
 }
