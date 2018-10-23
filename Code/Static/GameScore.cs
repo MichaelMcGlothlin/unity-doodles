150d7d4bf77369a0d8ee0ba98708c35243e6ff76
Michael McGlothlin <michael@kavlon.com>
2018-10-23 11:32:03 -0500

    Added GameScore for tracking score.

---
+++ Code/Static/GameScore.cs
@@ -0,0 +1,27 @@
+﻿namespace Kavlon {
+ public static class GameScore {
+  #region Events
+
+  public static event System.EventHandler ResetEvent;
+
+  private static void _onReset () => ResetEvent?.Invoke ( null, System.EventArgs.Empty );
+
+  public static event System.EventHandler<System.Int32> PointsAddedEvent;
+
+  private static void _onPointsAdded () => PointsAddedEvent?.Invoke ( null, Points );
+
+  #endregion
+
+  public static System.Int32 Points { get; private set; } = 0;
+
+  public static void AddPoints ( System.Int32 points ) {
+   Points += points;
+   _onPointsAdded ();
+  }
+
+  public static void ResetPoints () {
+   Points = 0;
+   _onReset ();
+  }
+ }
+}
