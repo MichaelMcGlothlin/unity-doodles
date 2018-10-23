0cb48292a57e90f9b28081e97a09b316571ae545
Michael McGlothlin <michael@kavlon.com>
2018-10-23 12:12:02 -0500

    Application added.

---
+++ Code/Static/Application.cs
@@ -0,0 +1,11 @@
+﻿namespace Kavlon {
+ public static class Application {
+  public static System.String DataPath => UnityEngine.Application.dataPath;
+
+  public static void Log ( System.String message ) => UnityEngine.Debug.Log ( message );
+
+  public static void Log ( System.Exception exception ) => UnityEngine.Debug.LogWarning ( exception.StackTrace );
+
+  public static void Quit () => UnityEngine.Application.Quit ();
+ }
+}
