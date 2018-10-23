5312a7ab3e03d2fe12caa52249e636a37cd871cd
Michael McGlothlin <michael@kavlon.com>
2018-10-23 14:14:17 -0500

    Added extension methods for rotation.

--- Code/Interface/ISkybox.cs
+++ Code/Interface/ISkybox.cs
@@ -4,4 +4,12 @@
 
   System.Single SkyboxRotation { get; set; }
  }
+
+ public static class SkyboxExtensions {
+  public static UnityEngine.Skybox GetSkybox ( this ISkybox skybox ) => ( skybox as UnityEngine.Component )?.GetComponent<UnityEngine.Skybox> ();
+
+  public static System.Single GetRotation ( this ISkybox skybox ) => skybox.Skybox.material.GetFloat ( "_Rotation" );
+
+  public static void SetRotation ( this ISkybox skybox, System.Single rotation ) => skybox.Skybox.material.SetFloat ( "_Rotation", rotation );
+ }
 }
