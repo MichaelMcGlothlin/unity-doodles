22bd9108d9ab1f03ec7dd27a62a3e0f97c38ca4e
Michael McGlothlin <michael@kavlon.com>
2018-10-24 11:22:27 -0500

    Added a static method to create Unival from a clamped value. Changed the implicit conversion to use this safer version.

--- Code/Struct/Unival.cs
+++ Code/Struct/Unival.cs
@@ -5,6 +5,11 @@
  public struct Unival {
   private System.Single _value;
 
+  /// <summary>
+  /// Create a Unival from a value. If value is out of range [0..1] an ArgumentOutOfRangeException
+  /// will be thrown.
+  /// </summary>
+  /// <param name="value">Value.</param>
   public Unival ( System.Single value ) {
    if ( ( value < 0 ) || ( value > 1 ) ) {
     throw new System.ArgumentOutOfRangeException ( nameof ( value ), "Unit intervals should be between zero and one." );
@@ -14,6 +19,15 @@
 
   public static implicit operator System.Single ( Unival unival ) => unival._value;
 
-  public static implicit operator Unival ( System.Single single ) => new Unival ( single );
+  public static implicit operator Unival ( System.Single single ) => Clamped ( single );
+
+  private static System.Single _clamp ( System.Single value ) => System.Math.Max ( 0, System.Math.Min ( 1, value ) );
+
+  /// <summary>
+  /// Creates a new Unival from a value that has been clamped to the correct range.
+  /// </summary>
+  /// <returns>Unival</returns>
+  /// <param name="value">Value.</param>
+  public static Unival Clamped ( System.Single value ) => new Unival ( _clamp ( value ) );
  }
 }
