8645a1944621d840503a5d203160c660e563008d
Michael McGlothlin <michael@kavlon.com>
2018-10-26 10:41:37 -0500

    Added support for 4, 5, 6, and 7th buttons. Added extension method to convert to keycodes.

--- Code/Enum/MouseButton.cs
+++ Code/Enum/MouseButton.cs
@@ -1,7 +1,41 @@
 ﻿namespace Kavlon {
  public enum MouseButton {
-  Primary = 0,
-  Secondary,
-  Middle
+  Primary = 0, // UnityEngine.KeyCode.Mouse0
+  Secondary, // UnityEngine.KeyCode.Mouse1
+  Middle, // UnityEngine.KeyCode.Mouse2
+  Button4, // UnityEngine.KeyCode.Mouse3
+  Button5, // UnityEngine.KeyCode.Mouse4
+  Button6, // UnityEngine.KeyCode.Mouse5
+  Button7 // UnityEngine.KeyCode.Mouse6
+ }
+
+ public static class MouseButtonExtensions {
+  public static UnityEngine.KeyCode ToKeyCode ( this MouseButton mouseButton ) {
+   switch ( mouseButton ) {
+    case MouseButton.Primary: {
+      return UnityEngine.KeyCode.Mouse0;
+     }
+    case MouseButton.Secondary: {
+      return UnityEngine.KeyCode.Mouse1;
+     }
+    case MouseButton.Middle: {
+      return UnityEngine.KeyCode.Mouse2;
+     }
+    case MouseButton.Button4: {
+      return UnityEngine.KeyCode.Mouse3;
+     }
+    case MouseButton.Button5: {
+      return UnityEngine.KeyCode.Mouse4;
+     }
+    case MouseButton.Button6: {
+      return UnityEngine.KeyCode.Mouse5;
+     }
+    case MouseButton.Button7: {
+      return UnityEngine.KeyCode.Mouse6;
+     }
+   }
+
+   return UnityEngine.KeyCode.None;
+  }
  }
 }
